32af0e2b42360c18e3065e424db75dd09463f68a
Cyril Hrubis <metan@ucw.cz>
2019-07-31 22:00:31 +0200

    file_size: Add terabytes.
    Signed-off-by: Cyril Hrubis <metan@ucw.cz>

--- src/gp_file_size.c
+++ src/gp_file_size.c
@@ -17,8 +17,10 @@ const char *gp_str_file_size(char *buf, size_t buf_len, size_t size)
 		snprintf(buf, buf_len, "%.2fKB", 1.00 * size/1024);
 	else if (size < 1024 * 1024 * 1024)
 		snprintf(buf, buf_len, "%.2fMB", 1.00 * size/(1024 * 1024));
-	else
+	else if (size < 1024 * 1024 * 1024 * 1024LLU)
 		snprintf(buf, buf_len, "%.2fGB", 1.00 * size/(1024 * 1024 * 1024));
+	else
+		snprintf(buf, buf_len, "%.2fTB", 1.00 * size/(1024 * 1024 * 1024 * 1024LLU));
 
 	return buf;
 }
