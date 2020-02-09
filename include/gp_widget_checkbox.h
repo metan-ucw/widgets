25adb07a72a8cc7bdbf7154d5daed8c8111b8289
Cyril Hrubis <metan@ucw.cz>
2020-02-09 18:10:39 +0100

    widgets/checkbox: Add missing function prototypes
    Signed-off-by: Cyril Hrubis <metan@ucw.cz>

--- include/gp_widget_checkbox.h
+++ include/gp_widget_checkbox.h
@@ -16,4 +16,8 @@ struct gp_widget *gp_widget_checkbox_new(const char *label,
                                          int (*on_event)(gp_widget_event *),
                                          void *priv);
 
+void gp_widget_checkbox_set(gp_widget *self, int val);
+
+void gp_widget_checkbox_toggle(gp_widget *self);
+
 #endif /* GP_WIDGET_CHECKBOX_H__ */
