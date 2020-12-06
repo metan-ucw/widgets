15ff72df69eb580bc1d02039a1dfdd77d7a5dab9
Cyril Hrubis <metan@ucw.cz>
2020-12-06 14:36:49 +0100

    button: Enable key-repeat events
    Signed-off-by: Cyril Hrubis <metan@ucw.cz>

--- src/gp_widget_button.c
+++ src/gp_widget_button.c
@@ -83,7 +83,7 @@ static int event(gp_widget *self, const gp_widget_render_ctx *ctx, gp_event *ev)
 
 	switch (ev->type) {
 	case GP_EV_KEY:
-		if (ev->code != GP_EV_KEY_DOWN)
+		if (ev->code == GP_EV_KEY_UP)
 			return 0;
 
 		switch (ev->val) {
