f47362bd6efe71516d59e2546b34b8651a39878d
Cyril Hrubis <metan@ucw.cz>
2020-12-12 17:02:51 +0100

    widgets: textbox: Add docs.
    Signed-off-by: Cyril Hrubis <metan@ucw.cz>

--- include/gp_widget_textbox.h
+++ include/gp_widget_textbox.h
@@ -37,31 +37,44 @@ enum gp_widget_text_box_flags {
 #define GP_TEXT_BOX_FILTER_INT "0123456789"
 #define GP_TEXT_BOX_FILTER_HEX "0123456789abcdefABCDEF"
 
-/*
- * @brief Creates a new textbox.
+/**
+ * @brief Allocate and initialize a textbox widget.
  *
  * @text     Initial textbox text.
  * @length   Expected text length, used for widget computation.
  * @filter   If set only characters from the string can be typed into the textbox.
  * @on_event Callback called on textbox event, e.g. key press.
- * @priv     User private pointer, unused by the widget library.
+ * @priv     User private pointer.
  * @flags    Mask of enum gp_widget_text_box_flags.
+ *
+ * @returns A textbox widget.
  */
-struct gp_widget *gp_widget_textbox_new(const char *text, unsigned int length,
-                                        const char *filter,
-                                        int (*on_event)(gp_widget_event *),
-                                        void *priv, int flags);
+gp_widget *gp_widget_textbox_new(const char *text, unsigned int length,
+                                 const char *filter,
+                                 int (*on_event)(gp_widget_event *),
+                                 void *priv, int flags);
 
+/**
+ * @brief A printf-like function to set a textbox value.
+ *
+ * @self A textbox widget.
+ * @fmt A printf-like format string.
+ * @... A printf-like parameters.
+ *
+ * @return A textbox string lenght or -1 on a failure.
+ */
 int gp_widget_textbox_printf(gp_widget *self, const char *fmt, ...)
                              __attribute__((format (printf, 2, 3)));
 
-/*
- * Clears the textbox text.
+/**
+ * @brief Clears the textbox text.
+ *
+ * @self A textbox widget.
  */
 void gp_widget_textbox_clear(gp_widget *self);
 
 
-/*
+/**
  * @brief Returns a textbox string.
  *
  * @self A textbox widget.
