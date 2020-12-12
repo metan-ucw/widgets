28d542ec05c9825201b88dea1c558e6a8f744238
Cyril Hrubis <metan@ucw.cz>
2020-12-12 15:31:23 +0100

    widgets: progress bar: Add docs.
    Signed-off-by: Cyril Hrubis <metan@ucw.cz>

--- include/gp_widget_pbar.h
+++ include/gp_widget_pbar.h
@@ -23,10 +23,31 @@ struct gp_widget_pbar {
 	enum gp_widget_pbar_type type;
 };
 
+/**
+ * @brief Allocate and initialize new progress bar widget.
+ *
+ * @val Initial progress bar value.
+ * @max Maximal progress bar value.
+ * @type Progress bar value type.
+ *
+ * @return A progress bar widget.
+ */
+gp_widget *gp_widget_pbar_new(float val, float max, enum gp_widget_pbar_type type);
+
+/**
+ * @brief Sets a progress bar value.
+ *
+ * @self A progress bar widget.
+ * @val New progress bar value.
+ */
 void gp_widget_pbar_set(gp_widget *self, float val);
 
+/**
+ * @brief Sets a progress bar maximum.
+ *
+ * @self A progress bar widget.
+ * @max New maximal progress bar value.
+ */
 void gp_widget_pbar_set_max(gp_widget *self, float max);
-
-gp_widget *gp_widget_pbar_new(float val, float max, enum gp_widget_pbar_type type);
 
 #endif /* GP_WIDGET_PBAR_H__ */
